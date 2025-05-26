84e7bb259c0c4fd3ea70398ed545123dc9306c0c
padelur <fadhlurrahman1909@gmail.com>
2025-05-26 16:42:05 +0700

    input movie

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -4,7 +4,7 @@
 <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="{{ $movie->cover_image }}" class="img-fluid rounded-start" alt="{{ $movie->title }}">
+                <img src="{{ asset('storage/'.$movie->cover_image)  }}" class="img-fluid rounded-start" alt="{{ $movie->title }}">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'homepage']);
 Route::get('/detail-movie/{id}/{slug}', [MovieController::class, 'show']);
-
+Route::get('/create-movie', [MovieController::class, 'create']);
+Route::post('/create-movie', [MovieController::class, 'store']);

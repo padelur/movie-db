c56cbd245f63f19bd906f425eceb498d4f015c58
padelur <fadhlurrahman1909@gmail.com>
2025-06-02 15:46:33 +0700

    login dan logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MovieController;
+use GuzzleHttp\Middleware;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,5 +12,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MovieController::class, 'homepage']);
 Route::get('/detail-movie/{id}/{slug}', [MovieController::class, 'show']);
-Route::get('/create-movie', [MovieController::class, 'create']);
-Route::post('/create-movie', [MovieController::class, 'store']);
+Route::get('/create-movie', [MovieController::class, 'create'])->middleware('auth');
+Route::post('/create-movie', [MovieController::class, 'store'])->Middleware('auth');
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
